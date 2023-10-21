e4a36b903c569b9ac5bbcd1ad28d6d1bf342fd1b
taeseokyang <ts.yang.0123@gmail.com>
2023-10-21 18:10:23 +0900

    feat

--- trapezoidal_rule.py
+++ trapezoidal_rule.py
@@ -21,19 +21,23 @@ for i in tms_datas:
 A = x[0]
 B = x[-1]
 # 델타 x
-n = 500
+n = 20
 delta_x = (B-A)//n
 
 # 보간
 interpolation = interp1d(x, y, kind='linear') # 선형 보간 함수 생성
 x = range(A, B)
 y = interpolation(x) # 보간한 y
+t_x = x[::delta_x]
+t_y = y[::delta_x]
 
 #사다리꼴 공식 계산
 area = 0
 for i in range(n):
     trapezoidal = delta_x*((y[i*delta_x]+y[(i+1)*delta_x])/2)
     area += trapezoidal
+    plt.vlines(x=i*delta_x, ymin=0, ymax=y[i*delta_x],color="blue",linewidth=0.5)
+    plt.vlines(x=(i+1)*delta_x ,ymin=0, ymax=y[(i+1)*delta_x],color="blue",linewidth=0.5)
 
 # 터미널 출력
 print("Area : {:.2f}(s)".format(area/1000))
@@ -42,7 +46,8 @@ print("Combustion Time : ",2.5,"s",sep="")
 
 # 그래프 출력
 plt.plot(x, y, color="red")
+plt.plot(t_x, t_y, color="blue",linewidth=0.5)
 plt.xlabel("Time(ms)")
 plt.ylabel("Newton")
-plt.title("TMS")
+plt.title("Trapezoidal Roul on TMS")
 plt.show()
